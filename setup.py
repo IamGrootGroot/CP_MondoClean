553839dbfe5b1a0874d5aa1a1afd9d2e3a8beef3
IamGrootGroot <maxencepelloux95@gmail.com>
2018-02-16 12:03:13 +0100

    SETUP

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
- """
+"""
  py2app/py2exe build script for MyApplication.
 
  Will automatically ensure that all build prerequisites are available
@@ -9,38 +9,36 @@
 
  Usage (Windows):
      python setup.py py2exe
- """
+"""
+import sys
+from setuptools import setup
 
- import sys
- from setuptools import setup
+DATA_FILES = [('',['images'])]
+OPTIONS = {}
+mainscript = 'UI.py'
 
- mainscript = 'UI.py'
-
- if sys.platform == 'darwin':
-     extra_options = dict(
-         setup_requires=['py2app'],
-         app=[mainscript],
-         # Cross-platform applications generally expect sys.argv to
-         # be used for opening files.
-         options=dict(py2app=dict(argv_emulation=True)),
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[mainscript],
+        data_files=DATA_FILES,
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True)),
      )
- elif sys.platform == 'win32':
+elif sys.platform == 'win32':
      extra_options = dict(
-         setup_requires=['py2exe'],
-         app=[mainscript],
+        setup_requires=['py2exe'],
+        app=[mainscript],
+        data_files=DATA_FILES,
+        options={'py2exe': OPTIONS}
      )
 else:
      extra_options = dict(
-         # Normally unix-like platforms will use "setup.py install"
-         # and install the main script as such
-         scripts=[mainscript],
+        scripts=[mainscript],
      )
 
 setup(
     name="MondoClean",
-    app=APP,
-    data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-    **extra_option
+    **extra_options
     )
